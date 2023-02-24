69babd17fc8594122bcca2829c29adc23fc6dc41
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-24 10:17:49 +0600

    learning firebase auth using email & pass form

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ-hZ9MU9Uhpw0MdyyLEd1etukgB8ohUw",
+    authDomain: "email-password-authentic-3f5bb.firebaseapp.com",
+    projectId: "email-password-authentic-3f5bb",
+    storageBucket: "email-password-authentic-3f5bb.appspot.com",
+    messagingSenderId: "1031554316632",
+    appId: "1:1031554316632:web:0e0d863073aed30cbe4d2d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
