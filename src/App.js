04e0ed64b0a52a53893945c197c55c4ab2932cd8
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-24 10:23:24 +0600

    created register component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
 import './App.css';
 import { EmailAuthProvider, getAuth } from 'firebase/auth';
 import app from './Firebase/firebase.init';
+import Register from './components/Register';
 
 const auth = getAuth(app);
-
-const handleRegister = (event) => {
-  event.preventDefault();
-  const email = event.target.email.value;
-  const password = event.target.password.value;
-  console.log(email, password);
-}
-
-const handleEmailBlur = (event) => {
-  console.log(event.target.value);
-}
-
-const handlePasswordBlur = (event) => {
-  console.log(event.target.value);
-}
-
 
 function App() {
   const emailProvider = new EmailAuthProvider();
 
   return (
     <div className="App">
-      <form onSubmit={handleRegister}>
-        <input onBlur={handleEmailBlur} type="email" name="email" id="" placeholder='Your Email' />
-        <br />
-        <input onBlur={handlePasswordBlur} type="password" name="password" id="" placeholder='Your Password' />
-        <br />
-        <button type="submit">Register</button>
-      </form>
+      <Register></Register>
     </div>
   );
 }
