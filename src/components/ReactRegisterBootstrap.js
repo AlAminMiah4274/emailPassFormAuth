6441142313cea11b019734cee211aeca178717c7
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-02-25 13:05:15 +0600

    learnt showing error message and success message

--- src/components/ReactRegisterBootstrap.js
+++ src/components/ReactRegisterBootstrap.js
@@ -8,21 +8,23 @@ const auth = getAuth(app);
 
 const ReactRegisterBootstrap = () => {
     const [passwordError, setPasswordError] = useState('');
+    const [success, setSuccess] = useState(false);
+
     const handleRgister = event => {
         event.preventDefault();
-        const email = event.target.email.value;
-        const password = event.target.password.value;
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
         console.log(email, password);
 
         if (!/(?=(.*[A-Z]){2,})/.test(password)) {
-            setPasswordError('Please provide at least two uppercase');
-            return;
+            setPasswordError('Password should be at least two uppercase');
         }
-        if (password.length < 6) {
-            setPasswordError('Password should be 8 characters');
+        if (!/(?=.*[!£$%&@#])/.test(password)) {
+            setPasswordError('Password should be at least one special character');
         }
-        if (!/(?=.*[!$£%&#@])/.test(password)) {
-            setPasswordError('Please provide a special character');
+        if (password.length > 6) {
+            setPasswordError('Password length should be 8 character');
         }
         setPasswordError('');
 
@@ -30,9 +32,14 @@ const ReactRegisterBootstrap = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                setSuccess(true);
+                form.reset();
             })
             .catch(e => {
                 console.error('The problem is:', e);
+                const message = e.message;
+                setPasswordError(message);
+                setSuccess(false);
             })
     }
 
@@ -50,6 +57,9 @@ const ReactRegisterBootstrap = () => {
                     <Form.Control type="password" name='password' placeholder="Password" required />
                 </Form.Group>
                 <p className='text-danger'>{passwordError}</p>
+                {success &&
+                    <p className='text-success'>User created successfuly</p>
+                }
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
